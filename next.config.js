ca65a7d8af7cb7659667be40fdf25308c775f9cd
CUBIC Café & Bar <cubicmza@gmail.com>
2026-09-12 16:55:35 -0300

    feat: redirigir glowupdeco.com → /cotizar/glowup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'glowupdeco.com' }],
+        destination: '/cotizar/glowup',
+        permanent: false,
+      },
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'www.glowupdeco.com' }],
+        destination: '/cotizar/glowup',
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'drive.google.com' },
